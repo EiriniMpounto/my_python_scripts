18be0d131c090edfa952bfc0622a4524464f77c1
Irene Bounto <irene@scircula.com>
2022-05-31 12:45:32 +0200

    Fixing the comment session

--- calculate_correlations_with_python.py
+++ calculate_correlations_with_python.py
@@ -21,7 +21,7 @@ from scipy.stats import spearmanr
 df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
 print(df)
 
-# setting up the data columns we want to correlate
+# SETTING UP THE DATA COLUMNS FOR CORRELATION
 data_a = df['A']
 data_b = df['B']
 
@@ -34,13 +34,15 @@ data_a_std = statistics.stdev(data_a)
 data_b_mean =statistics.mean(data_b)
 data_b_std = statistics.stdev(data_b)
 
-#preparing the data
-data1 = data_a_mean * data_a +data_a_std
+#PREPARING THE DATA
+data1 = data_a_mean * data_a + data_a_std
 data2 = data1 + (data_b_mean * data_b +data_b_std)
-# calculating the correlation
+
+# PEARSON CORRELATION
 corr, _ = pearsonr(data1, data2)
 print('Pearsons correlation: %.3f' % corr)
-# alternative way of calculating the correlation
+
+# SPEARMAN CORRELATION
 corr, _ = spearmanr(data_a, data_b)
 print('Spearmans correlation: %.3f' % corr)
 
